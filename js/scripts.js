bc0845c4c1b53637473f762e065e107fc14a770b
postolenko <denis.postolenko@gmail.com>
2020-04-29 21:13:41 +0300

    T

--- js/scripts.js
+++ js/scripts.js
@@ -25,32 +25,33 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
+    if( $(".slider_1").length > 0 ) {
+        $(".slider_1").not(".slick-initialized").slick({
             dots: true,
-            arrows: true,
-            autoplay: true,
+            arrows: false,
+            // autoplay: true,
             autoplaySpeed: 4000,
             speed: 1200,
-            slidesToShow: 4,
-            slidesToScroll: 1,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            rows: 2
             // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
         });
     }
 
