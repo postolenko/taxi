87af09155ff04cfea6a9d4c89cf50264052fdf52
postolenko <denis.postolenko@gmail.com>
2020-04-30 15:32:12 +0300

    T

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
+var parentBlock, dropdowmBlock;
 
 $(window).load(function() {
 
@@ -54,5 +55,88 @@ $(document).ready(function() {
             //   ]
         });
     }
+
+    if( $(".slider_2").length > 0 ) {
+        $(".slider_2").not(".slick-initialized").slick({
+            dots: true,
+            arrows: false,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
+        });
+    }
+
+    if( $(".slider_3").length > 0 ) {
+        $(".slider_3").not(".slick-initialized").slick({
+            dots: true,
+            arrows: false,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            rows: 2,
+            appendDots: $(".slider_3_controls")
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
+        });
+    }
+
+    // -------
+
+    $(".accordeon_item").each(function() {
+      if($(this).hasClass("active")) {
+        $(this).find(".accordeon_item_descript").css({
+          "display" : "block"
+        });
+      }
+    });
+
+    $(".accordeon_item_title").on("click", function(e) {
+      e.preventDefault();
+      parentBlock = $(this).closest(".accordeon_item");
+      dropdowmBlock = parentBlock.find(".accordeon_item_descript");
+      if(dropdowmBlock.is(":hidden")) {
+        dropdowmBlock.slideDown(300);
+        parentBlock.addClass("active");
+      } else {
+        dropdowmBlock.slideUp(300,function() {
+          parentBlock.removeClass("active");
+        });
+      }
+    });
 
 });
