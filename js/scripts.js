d370178523d91547ac71191995ccc6d8aebd0ba5
postolenko <denis.postolenko@gmail.com>
2020-05-02 00:42:30 +0300

    T

--- js/scripts.js
+++ js/scripts.js
@@ -26,39 +26,8 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".slider_1").length > 0 ) {
-        $(".slider_1").not(".slick-initialized").slick({
-            dots: true,
-            arrows: false,
-            // autoplay: true,
-            autoplaySpeed: 4000,
-            speed: 1200,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            rows: 2,
-            infinite: false
-            // fade: true,
-            // responsive: [
-            //     {
-            //       breakpoint: 900,
-            //       settings: {
-            //         slidesToShow: 2,
-            //         slidesToScroll: 2
-            //       }
-            //     },
-            //     {
-            //       breakpoint: 540,
-            //       settings: {
-            //         slidesToShow: 1,
-            //         slidesToScroll: 1
-            //       }
-            //     }
-            //   ]
-        });
-    }
-
-    if( $(".slider_2").length > 0 ) {
-        $(".slider_2").not(".slick-initialized").slick({
+    if( $(".resp_slider").length > 0 ) {
+        $(".resp_slider").not(".slick-initialized").slick({
             dots: true,
             arrows: false,
             // autoplay: true,
@@ -86,33 +55,66 @@ $(document).ready(function() {
         });
     }
 
+    if( $(".slider_1").length > 0 ) {
+        $(".slider_1").not(".slick-initialized").slick({
+            dots: true,
+            arrows: false,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            rows: 2,
+            infinite: false,
+            responsive: [
+                {
+                  breakpoint: 1170,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    rows: 2
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]
+        });
+    }
+
+    if( $(".slider_2").length > 0 ) {
+        $(".slider_2").not(".slick-initialized").slick({
+            dots: true,
+            arrows: false,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
+
     if( $(".slider_3").length > 0 ) {
         $(".slider_3").not(".slick-initialized").slick({
             dots: true,
             arrows: false,
-            // autoplay: true,
             autoplaySpeed: 4000,
             speed: 1200,
             slidesToShow: 2,
             slidesToScroll: 2,
             rows: 2,
-            appendDots: $(".slider_3_controls")
-            // responsive: [
-            //     {
-            //       breakpoint: 900,
-            //       settings: {
-            //         slidesToShow: 2,
-            //         slidesToScroll: 2
-            //       }
-            //     },
-            //     {
-            //       breakpoint: 540,
-            //       settings: {
-            //         slidesToShow: 1,
-            //         slidesToScroll: 1
-            //       }
-            //     }
-            //   ]
+            appendDots: $(".slider_3_controls"),
+            responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
         });
     }
 
